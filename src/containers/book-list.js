1f2989040a9369764a054419c455fb090cbe46e0
Marcelo Augusto <mekmay@hotmail.com>
2018-02-21 11:25:50 -0300

    Linking the react-redux.

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,8 +1,9 @@
 import React , {Component} from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
     renderList() {
-        return this.props.book.map((book) => {
+        return this.props.books.map((book) => {
             return (
                 <li key={book.title} className="list-group-item">{book.title}</li>
                 );
@@ -10,8 +11,20 @@ export default class BookList extends Component {
     }
 
     render(){
-        <ul className="list-group col-sm-4">
-            {this.renderList()}
-        </ul>
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
     }
 }
+
+function mapStateToProps(state) {
+    // Whatever is returned will show up as props
+    // inside of BookList
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);

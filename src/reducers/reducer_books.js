c2f13dc38acff0d1d069b212dda82773ecd7c5ab
Marcelo Augusto <mekmay@hotmail.com>
2018-02-22 14:52:28 -0300

    Adding book details container.

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {title: 'Book 1'},
-        {title: 'Book 2'},
-        {title: 'Book 3'},
-        {title: 'Book 4'}
+        {title: 'Book 1', pages: 155},
+        {title: 'Book 2', pages: 15},
+        {title: 'Book 3', pages: 11},
+        {title: 'Book 4', pages: 133}
     ]
 }
